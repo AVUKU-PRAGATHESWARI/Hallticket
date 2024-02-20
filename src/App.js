cd7c0fc4887f0f57516cac8592d34c6ee237685a
AVUKU-PRAGATHESWARI <pragatheswaria@gmail.com>
2024-02-20 18:51:29 +0530

    Fetching was successful

--- src/App.js
+++ src/App.js
@@ -3,14 +3,12 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import EntryContainer from './container/EntryContainer';
 import Hallticket from './components/Hallticket'; 
-import MyComponent from './components/Fetch';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<MyComponent/>} />
-        <Route path="/i" element={<EntryContainer />} />
+        <Route path="/" element={<EntryContainer />} />
         <Route path="/hallticket" element={<Hallticket />} />
       </Routes>
     </Router>
